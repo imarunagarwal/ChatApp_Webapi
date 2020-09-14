166ed907c753ad1973c2585ef1de2bc57eb1a2cc
Arun-Agarwal <Arun.Agarwal@coxautoinc.com>
2020-09-14 19:04:26 +0530

    Refactoring code base | Cors issue fix

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-var express = require('express');
-var app = express();
+var app = require('./server/server');
 var config = require('./server/config/config');
-var api = require('./server/api/api');
-var logger = require('./server/util/logger');
-var https = require('https');
 
-// connection with the db
-require('mongoose')
-    .connect(config.db.url,
-        {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        })
-    .then(() => logger.log('DB Connected!'))
-    .catch(err => logger.error('DB Connection Error: ' + err.message));
-
-if (config.seed) {
-    require('./server/util/seed');
-}
-
-//all the middle wares goes here.
-require('./server/middleware/appMiddleware')(app);
-
-app.use('/api', api);
-
-app.use(function (err, req, res, next) {
-    // if error thrown from jwt validation check
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).send('Invalid token');
-        return;
-    }
-    logger.error(err.stack);
-    res.status(500).send('Oops Internal Server Error');
-});
-
-var server = https.createServer({}, app);
-server.listen(config.port);
-// var server = app.listen(config.port);
+var server = app.listen(config.port);
 require('./server/socket')(server);
